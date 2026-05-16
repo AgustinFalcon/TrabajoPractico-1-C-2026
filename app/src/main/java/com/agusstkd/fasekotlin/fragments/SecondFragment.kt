a37d999d2c84c5737f5a7e2c967f33da909d0ebe
Agustin Falcon <agustinrobertofalcon@gmail.com>
2026-05-15 21:02:32 -0300

    live data y material design - patron observer

--- app/src/main/java/com/agusstkd/fasekotlin/fragments/SecondFragment.kt
+++ app/src/main/java/com/agusstkd/fasekotlin/fragments/SecondFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.agusstkd.fasekotlin.R
 import com.agusstkd.fasekotlin.databinding.FragmentSecondBinding
 
@@ -24,6 +28,43 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val secondViewModel = ViewModelProvider(this).get(SecondViewModel::class.java)
+
+        binding.etName.addTextChangedListener { email ->
+            secondViewModel.validateEmail(email = email.toString())
+        }
+
+        binding.etPassword.addTextChangedListener { password ->
+            secondViewModel.validatePassword(password = password.toString())
+        }
+
+        secondViewModel.viewState.observe(viewLifecycleOwner, Observer { state ->
+            when (state) {
+                SecondStatesSealed.SuccessEmail -> {
+                    binding.layoutEmail.error = null
+                }
+
+                SecondStatesSealed.ErrorEmail -> {
+                    binding.layoutEmail.error = "Formato de email invalido"
+                }
+
+                SecondStatesSealed.SuccessPassword -> {
+                    binding.layoutPassword.error = null
+                }
+
+                is SecondStatesSealed.ErrorPassword -> {
+                    binding.layoutPassword.error = "${state.password.length}/4"
+                }
+
+                SecondStatesSealed.ErrorButton -> {
+                    binding.btnNext.isEnabled = false
+                }
+
+                SecondStatesSealed.SuccessButton -> {
+                    binding.btnNext.isEnabled = true
+                }
+            }
+        })
     }
 
 
---
+++ app/src/main/java/com/agusstkd/fasekotlin/fragments/SecondViewModel.kt
@@ -0,0 +1,72 @@
+package com.agusstkd.fasekotlin.fragments
+
+import androidx.core.util.PatternsCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.agusstkd.fasekotlin.Persona
+
+class SecondViewModel : ViewModel() {
+    //var viewState = MutableLiveData<SecondStateEnum>()
+    var viewState = MutableLiveData<SecondStatesSealed>()
+
+    private var email: String = ""
+    private var password: String = ""
+
+
+    fun validateEmail(email: String) {
+        this.email = email
+        if (email.isNotBlank() && PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()) {
+             //viewState.value = SecondStateEnum.SUCCESS_EMAIL
+            viewState.value = SecondStatesSealed.SuccessEmail
+
+        } else {
+            //viewState.value = SecondStateEnum.ERROR_EMAIL
+            viewState.value = SecondStatesSealed.ErrorEmail
+        }
+
+        validateButtons()
+    }
+
+    fun validatePassword(password: String) {
+        this.password = password
+        if (password.isNotBlank() && password.length > 3) {
+            //viewState.value = SecondStateEnum.SUCCESS_PASSWORD
+            viewState.value = SecondStatesSealed.SuccessPassword
+
+        } else {
+            //viewState.value = SecondStateEnum.ERROR_PASSWORD
+            viewState.value = SecondStatesSealed.ErrorPassword(password = password)
+        }
+
+        validateButtons()
+    }
+
+    fun validateButtons() {
+        if ((email.isNotBlank() && PatternsCompat.EMAIL_ADDRESS.matcher(email).matches())
+            && (password.isNotBlank() && password.length > 3)) {
+
+            viewState.value = SecondStatesSealed.SuccessButton
+        } else {
+            viewState.value = SecondStatesSealed.ErrorButton
+        }
+    }
+}
+
+enum class SecondStateEnum {
+    SUCCESS_EMAIL,
+    ERROR_EMAIL,
+    SUCCESS_PASSWORD,
+    ERROR_PASSWORD,
+    ERROR_BUTTON,
+    SUCCESS_BUTTON
+}
+
+sealed class SecondStatesSealed() {
+    object SuccessEmail: SecondStatesSealed()
+    object ErrorEmail: SecondStatesSealed()
+    object SuccessPassword: SecondStatesSealed()
+    data class ErrorPassword(val password: String): SecondStatesSealed()
+    object ErrorButton: SecondStatesSealed()
+    object SuccessButton: SecondStatesSealed()
+}
